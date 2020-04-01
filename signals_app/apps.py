f2093a9aa30d24f5ba328f2f0ee0a35bb0dcf837
iCarlo <jcbbasilio28@gmail.com>
2020-04-01 14:51:18 +0800

    signals functionality deployed

--- signals_app/apps.py
+++ signals_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class SignalsAppConfig(AppConfig):
     name = 'signals_app'
+
+    def ready(self):
+        import signals_app.signals
--- signals_app/models.py
+++ signals_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -10,20 +9,4 @@ class Profile(models.Model):
     phone = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
-        return self.first_name
-
-
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-        print("Profile Created!")
-
-post_save.connect(create_profile, sender=User)
-
-
-def update_profile(sender, instance, created, **kwargs):
-    if created is False:
-        instance.profile.save()
-        print("Profile Updated!")
-
-post_save.connect(update_profile, sender=User)
+        return str(self.user)
